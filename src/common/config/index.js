f91c6d2755bef4726c384618fe318958e2c51699
Jack <hantmac@outlook.com>
2019-03-19 12:52:03 +0800

    Update index.js

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -1,6 +1,6 @@
 //本地存储的前缀
-export const storagePrefix = 'gerapy'
+export const storagePrefix = 'collyFront'
 //base64加密前缀
-export const base64Prefix = 'gerapy'
+export const base64Prefix = 'collyFront'
 //接入服务器接口地址根目录
 export const serverBase_url = '/'
